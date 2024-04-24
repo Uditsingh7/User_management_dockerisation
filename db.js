14b6466228d5c76ca67657bc4d8a785463252732
Uditsingh-epi <uditsingh.thakur@extrapreneursindia.com>
2024-04-25 00:28:10 +0530

    update: updated github actions config

--- db.js
+++ db.js
@@ -2,15 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 async function connectDb() {
     try {
-        let uri;
-
-        if (process.env.NODE_ENV === 'test') {
-            // Use a mock MongoDB URI for testing
-            uri = 'mongodb://localhost:27017/testDB';
-        } else {
-            // Use the MongoDB URI from the environment variables
-            uri = process.env.MONGODB_URI;
-        }
+        const uri = process.env.MONGODB_URI;
         await mongoose.connect(uri);
         console.log('Connected to MongoDB');
     }
