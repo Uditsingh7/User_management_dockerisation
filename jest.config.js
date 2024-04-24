b19602997b65e66e1d07b6474fcfa26df8ea5e2f
Uditsingh-epi <uditsingh.thakur@extrapreneursindia.com>
2024-04-25 00:11:43 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,4 @@ module.exports = {
     testTimeout: 10000, // Increase timeout to 10 seconds
     verbose: true,
     detectOpenHandles: true, // Detect open handles to troubleshoot asynchronous operations
-    setupFilesAfterEnv: ['./jest.setup.js'], // Optional: Add setup file for custom Jest configurations
 };
